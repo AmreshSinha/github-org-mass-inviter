551899677aa123597ea9567676776c328f2c2cac
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-19 23:47:27 +0530

    Add more customisation for ease of use

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ async function verifyGithubId(githubID) {
 
 const TEAM_ID = parseInt(process.env.TEAM_ID);
 
-async function sendOrgInvite(githubID) {
+async function sendOrgInvite(githubUser, githubID) {
+    var status;
     const response = await fetch('https://api.github.com/orgs/Coding-Club-IITG/invitations', {
             method: 'POST',
             headers: { 'Accept': 'application/vnd.github.v3+json', 'Authorization': `token ${process.env.GITHUB_TOKEN}`},
@@ -28,11 +29,14 @@ async function sendOrgInvite(githubID) {
                 'invitee_id': githubID,
                 'team_ids': [TEAM_ID]
             })
-    }).then(res => res.json()).then(json => {
-        if (json.status === 201) {
-            console.log(`${githubID} invited to the Team ID ${TEAM_ID} successfully`);
-        } else {
-            console.log(json);
+    })
+    .then(res => {status = res.status;return res.json()}).then(json => {
+        if (status === 201) {
+            console.log('\x1b[32m%s\x1b[0m', `${githubUser} invited to the Team ID ${TEAM_ID} successfully`);
+        } else if (status === 422) {
+            console.log('\x1b[31m%s\x1b[0m', json.errors[0].message);
+        } else if (status === 404) {
+            console.log('\x1b[31m%s\x1b[0m', `Resource not found`);
         }
     }).catch(err => console.log(err));
 }
@@ -41,10 +45,13 @@ for (let i = 0; i < users.length; i++) {
     const githubID = users[i];
     const verified = await verifyGithubId(githubID);
     
-    console.log(verified);
+    // console.log(verified);
+	process.stdout.write(`${i+1}. User: ${users[i]}, Status: `)
 
     if (verified) {
-        await sendOrgInvite(verified);
+        await sendOrgInvite(users[i], verified);
+    } else {
+        console.log('\x1b[31m%s\x1b[0m', `${users[i]} not found`);
     }
     
 }
